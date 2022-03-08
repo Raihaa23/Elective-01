f1a137518ce14a38804b48e5c3e78ae0a6b30f7f
Ryan Cedullo <ryan.cedullo@ciit.edu.ph>
2022-03-08 10:24:58 +0800

    added parameter for Test script

--- EMC/Assets/Scripts/Test.cs
+++ EMC/Assets/Scripts/Test.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//Can Code here for U.I Design
 public class Test : MonoBehaviour
 {
     // Start is called before the first frame update
